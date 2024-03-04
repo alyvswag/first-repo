cc1a4779a0d759d7e39e5d7d9262b4d7cae3e249
Talib Aliyev <133005678+alyvswag@users.noreply.github.com>
2024-03-04 19:28:08 +0400

    added first message

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome! salam aeee");
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-            System.out.println("Sakam kisi ");
-        }
+
+        System.out.println("HELLO EL OGLU");
+
     }
 }
